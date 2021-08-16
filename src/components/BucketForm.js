e8412638eabe374f9e2f396ff770770ceddf866b
Junny <tajo0215@gmail.com>
2021-08-16 15:04:14 -0700

    bucketForm refactored

--- src/components/BucketForm.js
+++ src/components/BucketForm.js
@@ -1,5 +1,38 @@
 import React, { useState } from "react";
 
+function Dropdown({ eagerness, setEagerness }) {
+  return (
+    <div className="dropdown">
+      <button className={`dropbtn ${eagerness}`}>
+        {eagerness || "Priority"}
+      </button>
+      <div className="dropdown-content">
+        {/* TODO: Add an onClick events that will set the corresponding eagerness level to "low", "medium", or "high" */}
+        <p onClick={() => setEagerness("high")}>Must do</p>
+        <p onClick={() => setEagerness("medium")}>Want to do</p>
+        <p onClick={() => setEagerness("low")}>Take it or leave it</p>
+      </div>
+    </div>
+  );
+}
+
+function TextInput({
+  value,
+  onChange,
+  placeholder = "Add to your bucket list",
+}) {
+  return (
+    <input
+      type="text"
+      placeholder={placeholder}
+      value={value}
+      name="text"
+      className="bucket-input"
+      onChange={onChange}
+    ></input>
+  );
+}
+
 function BucketForm(props) {
   const [input, setInput] = useState(props.edit?.value || "");
   const [eagerness, setEagerness] = useState(props.edit?.eagerness || "");
@@ -23,55 +56,28 @@ function BucketForm(props) {
 
   // First we check to see if "edit" prop exists. If not, we render the normal form
   // If the prop "edit" exists, we know to render the update form instead
-  return !props.edit ? (
+  if (props.edit) {
+    return (
+      <div>
+        <h3>Update entry: {props.edit.value}</h3>
+        <form className="bucket-form" onSubmit={handleSubmit}>
+          <TextInput
+            value={input}
+            onChange={handleChange}
+            placeholder={props.edit.value}
+          />
+          <Dropdown setEagerness={setEagerness} eagerness={eagerness} />
+          <button className="bucket-button">Update</button>
+        </form>
+      </div>
+    );
+  }
+  return (
     <div>
       <form className="bucket-form" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Add to your bucket list"
-          value={input}
-          name="text"
-          className="bucket-input"
-          onChange={handleChange}
-        ></input>
-        <div className="dropdown">
-          <button className={`dropbtn ${eagerness}`}>
-            {eagerness || "Priority"}
-          </button>
-          <div className="dropdown-content">
-            {/* TODO: Add an onClick events that will set the corresponding eagerness level to "low", "medium", or "high" */}
-            <p onClick={() => setEagerness("high")}>Must do</p>
-            <p onClick={() => setEagerness("medium")}>Want to do</p>
-            <p onClick={() => setEagerness("low")}>Take it or leave it</p>
-          </div>
-        </div>
+        <TextInput value={input} onChange={handleChange} />
+        <Dropdown setEagerness={setEagerness} eagerness={eagerness} />
         <button className="bucket-button">Add bucket list item</button>
-      </form>
-    </div>
-  ) : (
-    <div>
-      <h3>Update entry: {props.edit.value}</h3>
-      <form className="bucket-form" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder={props.edit.value}
-          value={input}
-          name="text"
-          className="bucket-input"
-          onChange={handleChange}
-        ></input>
-        <div className="dropdown">
-          <button className={`dropbtn ${eagerness}`}>
-            {eagerness || "Priority"}
-          </button>
-          <div className="dropdown-content">
-            {/* TODO: Add an onClick events that will set the corresponding eagerness level to "low", "medium", or "high" */}
-            <p onClick={() => setEagerness("high")}>Must do</p>
-            <p onClick={() => setEagerness("medium")}>Want to do</p>
-            <p onClick={() => setEagerness("low")}>Take it or leave it</p>
-          </div>
-        </div>
-        <button className="bucket-button">Update</button>
       </form>
     </div>
   );
